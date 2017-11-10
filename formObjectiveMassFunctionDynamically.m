f2cd12f7be82693419817f43ea7e2ae550497705
Trexter <sheridak@purdue.edu>
2017-11-10 15:05:54 -0500

    fix name

--- formObjectiveMassFunctionDynamically.m
+++ formObjectiveMassFunctionDynamically.m
@@ -1,4 +1,4 @@
-function [final_fn] = formObjectionMassFunctionDynamically(wing)
+function [final_fn] = formObjectiveMassFunctionDynamically(wing)
 %dynamically form a mass function to be minimized
 %the order of x is like this: stringer area, skin thickness, [skin stringer
 %areas], next stringer area, next skin thickness, [next skin stringer
