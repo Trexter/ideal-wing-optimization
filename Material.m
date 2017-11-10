474d2a05c0dc87d1585f1cf1500e3b78a33adab2
Trexter <sheridak@purdue.edu>
2017-11-10 13:48:46 -0500

    added 5 material choices

--- Material.m
+++ Material.m
@@ -3,10 +3,10 @@ classdef Material
     
     properties
         type=1 % an internal variable which acts as the index when looking up the material
-        % materials: Al6061, 
-        youngs_modulus_list = [68.9e9] % Pa
-        yield_strength_list = [276e6] % Pa
-        density_list = [2700] % kg/m^3
+        % materials: Al6061, Al7075, Al5052, Al2024, Ti6Al4V
+        youngs_modulus_list = [68.9e9, 71.7e9, 70.3e9, 73.1e9, 113.8e9] % Pa
+        yield_strength_list = [276e6, 503e6, 214e6, 324e6, 880e6] % Pa
+        density_list = [2700, 2810, 2680, 2780, 4430] % kg/m^3
     end
     
     methods
