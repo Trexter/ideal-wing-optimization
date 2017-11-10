c5aa792a190ef5d6dd5aea2446e19999d1981656
Trexter <sheridak@purdue.edu>
2017-11-10 16:23:04 -0500

    i think that i figured out how to fix it

--- computeWingMass.m
+++ computeWingMass.m
@@ -1,13 +1,9 @@
 function [mass] = computeWingMass(wing)
 %Simply computes the mass of this dynamic wing per meter length in kg
 
-mass = 0;
+mass_fn = formObjectiveMassFunctionDynamically(wing);
 
-%start with stringer1
-mass = mass + wing.stringer1.area * wing.stringer1.material.getDensity;
-
-%top left skin and its stringers
-mass = mass + wing.skin_tl.thickness * wing.skin_tl.length * wing.skin_tl.material.getDensity;
+mass = mass_fn(wing2ThicknessVector(wing));
 
 end
 
