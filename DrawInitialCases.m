426924fd0eaa26a8a0407cba34d85a6ff7a6ac81
Brad Lock <33606195+bdubyalock@users.noreply.github.com>
2017-11-12 23:54:44 -0500

    Updated Pictures to match wing cases. Added drawing function

---
+++ DrawInitialCases.m
@@ -0,0 +1,10 @@
+%Draw Case 1, Case 2, Case 3
+subplot(3,1,1);
+AAE_352_Proj1_Draw(Case1_wing);
+title('Case 1: 2 spars, 4 spar caps, 4 stringers');
+subplot(3,1,2);
+AAE_352_Proj1_Draw(Case2_wing);
+title('Case 2: 2 spars, 4 spar caps, 6 stringers');
+subplot(3,1,3);
+AAE_352_Proj1_Draw(Case3_wing);
+title('Case 3: 2 spars, 4 spar caps, 8 stringers');
